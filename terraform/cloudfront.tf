5a04cf372ae59662e97046de754fb090ffd07143
Nicholas Wade <Nicholas.Wade@phila.gov>
2019-12-05 15:33:40 -0500

    tf config for dpd aws tenant

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,98 +1,16 @@
-resource "aws_s3_bucket" "dpd_document_bucket" {
-  bucket = "${var.project}-${var.environment}-ui"
-  acl    = "private"
-}
+# logs.tf
 
-locals {
-  s3_origin_id = "${var.project}-${var.environment}-ui"
-}
+# Set up CloudWatch group and log stream and retain logs for 30 days
+resource "aws_cloudwatch_log_group" "dpd_document_api_log_group" {
+  name              = "${var.project}-${var.environment}-api"
+  retention_in_days = 30
 
-resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-  comment = "For DPD-Document-API"
-}
-
-resource "aws_cloudfront_distribution" "dpd_document_cf_dist" {
-  origin {
-    domain_name = "${aws_s3_bucket.dpd_document_bucket.bucket_regional_domain_name}"
-    origin_id   = "${local.s3_origin_id}"
-
-    s3_origin_config {
-      origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
-    }
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = ""
-  default_root_object = "index.html"
-
-  custom_error_response {
-      error_code = 404
-      error_caching_min_ttl = 300
-      response_code = 200
-      response_page_path = "/"
-  }
-
-  aliases = ["${var.cf_aliases}"]
-
-  default_cache_behavior {
-    allowed_methods  = ["HEAD", "GET"]
-    cached_methods   = ["HEAD", "GET"]
-    target_origin_id = "${local.s3_origin_id}"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
-  }
-
-  price_class = "PriceClass_100"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = "${var.certificate_arn}"
-    ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv1"
+  tags = {
+    Name = "dpd-document-api-log-group"
   }
 }
 
-# bucket policy
-data "aws_iam_policy_document" "s3_policy" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.dpd_document_bucket.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
-    }
-  }
-
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = ["${aws_s3_bucket.dpd_document_bucket.arn}"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "dpd_document_bucket_pol" {
-  bucket = "${aws_s3_bucket.dpd_document_bucket.id}"
-  policy = "${data.aws_iam_policy_document.s3_policy.json}"
+resource "aws_cloudwatch_log_stream" "dpd_document_api_log_stream" {
+  name           = "dpd-document-api-log-stream"
+  log_group_name = "${aws_cloudwatch_log_group.dpd_document_api_log_group.name}"
 }
--- terraform/services.tf
+++ terraform/services.tf
@@ -1,61 +1,50 @@
 # cluster
 resource "aws_ecs_cluster" "ecs_cluster" {
-    name = "${var.project}-${var.environment}-cluster"
+  name = "${var.project}-${var.environment}-cluster"
 }
 
 # tasks
 # API
-module "dpd_document_api_td" {
-  source                    = "app.terraform.io/philadelphia/ecs-task/aws"
-  version                   = "0.1.15"
-  #vpc                       = "${var.vpc_id}"
-  dept                      = "${var.department}"
-  env                       = "${var.environment}"
-  proj                      = "${var.project}"
-  infra                     = "api"
-  region                    = "${var.aws_region}"
-  temp_file                 = "${file("task_definition/dpd-document-api.json")}"
-  task_role                 = "${aws_iam_role.ecs_task_role.arn}"
-  req_comp                  = ["FARGATE"]
-  launch_type               = "FARGATE"
-  ecs_clus                  = "${aws_ecs_cluster.ecs_cluster.id}"
-  ecs_clus2                 = "${aws_ecs_cluster.ecs_cluster.name}"
-  ecs_role                  = "${aws_iam_role.ecs_service_role.name}"
-  net_mode                  = "awsvpc"
-  task_subnets              = ["${var.priv_subnet1}", "${var.priv_subnet2}", "${var.priv_subnet3}", "${var.priv_subnet4}"]
-  task_security             = ["${aws_security_group.public.id}", "${aws_security_group.dpd_document_ecs.id}"]
-  desired                   = 1
-  depl_max                  = 200
-  depl_min                  = 100
-  health_grace              = 300
-  #depends                   = ["${aws_iam_role_policy.ecs_service_role_policy}"]
-  place_type                = "spread"
-  place_field               = "host"
-  depl_type                 = "ECS"
-  tar_grp                   = "${module.dpd_document_api_tg.tgb_arn}"
-  cont_port                 = 80
-  cpu_val                   = 256
-  mem_val                   = 1024
-  adj_type                  = "ChangeInCapacity"
-  adj_val1                  = 1
-  adj_val2                  = -1
-  scale_cool                = 300
-  scale_pol                 = "SimpleScaling"
-  comp_op1                  = "GreaterThanOrEqualToThreshold"
-  comp_op2                  = "LessThanOrEqualToThreshold"
-  per_eval1                 = 4
-  per_eval2                 = 4
-  met_name                  = "CPUUtilization"
-  name_spc                  = "AWS/ECS"
-  per_len                   = 60
-  met_stat                  = "Average"
-  per_alrm1                 = 2
-  per_alrm2                 = 3
-  met_thr1                  = 80
-  met_thr2                  = 50
-  #dim_name                  = "ServiceName"
-  #dim_val                   = "${aws_ecs_service.this.name}"
-  min_count                 = "${var.min_task_count}"
-  max_count                 = "${var.max_task_count}"
-  ecs_scale_role            = "arn:aws:iam::102658671810:role/ecsAutoscaleRole"
+
+data "template_file" "dpd_document_api" {
+  template = "${file("task_definition/dpd-document-api.json")}"
+  vars = {
+    project          = "${var.project}"
+    environment      = "${var.environment}"
+    infra            = "api"
+    log_group_name   = "${aws_cloudwatch_log_group.dpd_document_api_log_group.name}"
+    log_group_region = "${var.aws_region}"
+  }
+}
+
+resource "aws_ecs_task_definition" "dpd_document_api_task" {
+  family                   = "dpd-document-api-task"
+  execution_role_arn       = "${aws_iam_role.ecs_task_role.arn}"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 1024
+  container_definitions    = "${data.template_file.dpd_document_api.rendered}"
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "document-api-service"
+  cluster         = "${aws_ecs_cluster.ecs_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.dpd_document_api_task.arn}"
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = ["${aws_security_group.public.id}", "${aws_security_group.dpd_document_ecs.id}"]
+    subnets          = ["${var.priv_subnet1}", "${var.priv_subnet2}", "${var.priv_subnet3}", "${var.priv_subnet4}"]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.dpd_front_end.arn}"
+    container_name   = "${var.project}-${var.environment}-api"
+    container_port   = 80
+  }
+
+  depends_on = [aws_alb_listener.dpd_document_public2, aws_iam_role.ecs_task_role]
 }
