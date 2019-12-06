0efb57639bd16293f8bde2f52a8b4868da4161ae
Nicholas Wade <Nicholas.Wade@phila.gov>
2019-12-06 12:22:21 -0500

    security group to only allow city/gatekeeper

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,7 +1,7 @@
 # alb public security group
 resource "aws_security_group" "public" {
   name        = "${var.project}-${var.environment}-public"
-  description = "Allow inbound traffic from the internet"
+  description = "Allow inbound traffic from City machines and gatekeeper"
   vpc_id      = "${var.vpc_id}"
 
   ingress {
@@ -9,8 +9,8 @@ resource "aws_security_group" "public" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "allow https from the internet"
+    cidr_blocks = "${var.CityTrafficCIDRBlocks}"
+    description = "allow https from the City machines and gatekeeper"
   }
 
   ingress {
@@ -18,8 +18,8 @@ resource "aws_security_group" "public" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "allow https from the internet"
+    cidr_blocks = "${var.CityTrafficCIDRBlocks}"
+    description = "allow http from the City machines and gatekeeper"
   }
 
   egress {
@@ -81,48 +81,5 @@ resource "aws_security_group" "dpd_document_ecs" {
 
   tags = {
     Name = "${var.project}-${var.environment}-ecs"
-  }
-}
-
-
-
-# allow access to the database from the api servers
-# this is not automatically applied to the database
-resource "aws_security_group" "dpd_document_database" {
-  name        = "${var.project}-${var.environment}-db"
-  description = "Allow inbound traffic from servers"
-  vpc_id      = "${var.vpc_id}"
-
-  ingress {
-    # TLS (change to whatever ports you need)
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = ["10.8.100.0/22"]
-    description = "allow connections from citynet"
-  }
-
-  ingress {
-    # TLS (change to whatever ports you need)
-    from_port       = 1521
-    to_port         = 1521
-    protocol        = "tcp"
-    security_groups = ["${aws_security_group.dpd_document_ecs.id}"]
-    description     = "allow connections from the api servers"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Name = "${var.project}-${var.environment}-database"
   }
 }
