9a19acb0698f0706a1d01a6c51c5451c573ba3a6
Nicholas Wade <Nicholas.Wade@phila.gov>
2020-07-22 12:24:01 -0400

    prod infra for new endpoint

--- terraform/prod/ecs-service-role.tf
+++ terraform/prod/ecs-service-role.tf
@@ -1,21 +1,21 @@
 resource "aws_iam_role" "ecs_service_role" {
-    name                = "${var.project}-${var.environment}-service-role"
-    path                = "/"
-    assume_role_policy  = "${data.aws_iam_policy_document.ecs_service_policy.json}"
+  name               = "${var.project}-${var.environment}-service-role"
+  path               = "/"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_service_policy.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_service_role_attachment" {
-    role       = "${aws_iam_role.ecs_service_role.name}"
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+  role       = "${aws_iam_role.ecs_service_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
 data "aws_iam_policy_document" "ecs_service_policy" {
-    statement {
-        actions = ["sts:AssumeRole"]
+  statement {
+    actions = ["sts:AssumeRole"]
 
-        principals {
-            type        = "Service"
-            identifiers = ["ecs.amazonaws.com"]
-        }
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com"]
     }
+  }
 }
